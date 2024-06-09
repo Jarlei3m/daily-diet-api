29a5b31559e57cfbe18115fa162e3e135ea87556
Járlei Rodrigues <jarlei_rodrigues@outlook.com>
2024-06-09 10:30:08 -0300

    feat: Validations fix and get all meals api added

--- app.py
+++ app.py
@@ -12,18 +12,21 @@ db.init_app(app)
 @app.route('/meal', methods=['POST'])
 def register_meal():
   data = request.json
+
   name = data["name"]
+  on_diet = data["on_diet"]
+
+  if not name or on_diet is None:
+    return jsonify({"message": "Name and on diet fields are required!"}), 400
+
   description = data["description"]
   created_at = datetime.now(timezone.utc)
-  on_diet = data["on_diet"]
   
-  if name and on_diet:
-    meal = Meal(name=name, description=description, created_at=created_at, on_diet=on_diet)
-    db.session.add(meal)
-    db.session.commit()
-    return jsonify({"message": "Meal has been successfully registered!"})
+  meal = Meal(name=name, description=description, created_at=created_at, on_diet=on_diet)
+  db.session.add(meal)
+  db.session.commit()
+  return jsonify({"message": "Meal has been successfully registered!"})
   
-  return jsonify({"message": "Required fields are not filled"}), 400
 
 @app.route('/meal/<int:id_meal>', methods=['PUT'])
 def update_meal(id_meal):
@@ -35,7 +38,7 @@ def update_meal(id_meal):
   name = data.get('name')
   on_diet = data.get('on_diet')
 
-  if name is None or on_diet is None:
+  if not name or on_diet is None:
     return jsonify({"message": "Name and on diet fields are required!"}), 400
 
   meal.name = name
@@ -57,6 +60,19 @@ def delete_meal(id_meal):
   db.session.commit()
   return jsonify({"message": f"Meal {id_meal} was successfully removed!"})
 
+@app.route('/meal', methods=['GET'])
+def get_meals():
+  meals = Meal.query.all()
+  if not meals:
+    return jsonify({"message": "No meals registered at the moment"})
+  
+  meals_list = [meal.to_dict() for meal in meals]
+  output = {
+    "meals": meals_list,
+    "total_meals": len(meals_list)
+  }
+
+  return jsonify(output)
 
 if __name__ == '__main__':
   app.run(debug=True)
--- models/meal.py
+++ models/meal.py
@@ -4,6 +4,14 @@ class Meal(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(80), unique=True, nullable=False)
   description = db.Column(db.String(300))
-  on_diet = db.Column(db.Boolean(), nullable={False})
+  on_diet = db.Column(db.Boolean(), nullable=False)
   created_at = db.Column(db.DateTime)
   updated_at = db.Column(db.DateTime)
+
+  def to_dict(self):
+    return {
+      "id": self.id,
+      "name": self.name,
+      "description": self.description,
+      "on_diet": self.on_diet
+    }
